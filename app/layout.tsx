11be886cc167e6ea4c2b2acb490dd073ef831b90
julianahdzdiaz <julianahdzdiaz@gmail.com>
2026-02-06 13:58:03 -0500

    fix: valid root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
-export default function
-RootLayout({ children }: { children:
-React.ReactNode }) {
-    return (
-        <html lang="es">
-            <body>{children}</body>
-        </html>
-    )
+export const metadata = {
+  title: "Chat Portafolio",
+  description: "Portafolio en formato chat",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
 }
